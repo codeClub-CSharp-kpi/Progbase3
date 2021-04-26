51a7fc22969d26c2b8adc10196ef3b40f7077aaa
KhomenkoMac <m.khomenko03@gmail.com>
2021-04-26 12:55:06 +0300

    Models moved to spec directory

---
+++ Progbase3/Generator/models/Film.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generator
+{
+	[Table("Films")]
+	class Film
+	{
+		[Key]
+		public int Id { get; set; }
+		
+		[Required]
+		[StringLength(50)]
+		public string Title { get; set; }
+		
+		[Required]
+		[Column(TypeName="float")]
+		public double Rate { get; set; }
+
+		[Required]
+		[Column(TypeName = "date")]
+		public DateTime OfficialReleaseDate { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		public string Slogan { get; set; } // 50 symbols
+
+		[Required]
+		[StringLength(250)]
+		public string StoryLine { get; set; } // 250 symbols
+
+		public virtual ICollection<Review> Reviews { get; set; }
+	}
+}
---
+++ Progbase3/Generator/models/Review.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Generator.models
+{
+	[Table("Reviews")]
+	class Review
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		public string Title { get; set; } // 50 symbs
+
+		[Required]
+		[Column(TypeName="bit")]
+		public bool isPositive { get; set; }
+
+		[Required]
+		[StringLength(250)]
+		public string ReviewText { get; set; } // 250 symbs
+		
+		[ForeignKey("Film")]
+		public int FilmId { get; set; }
+		public virtual Film Film { get; set; }
+		
+	}
+}
