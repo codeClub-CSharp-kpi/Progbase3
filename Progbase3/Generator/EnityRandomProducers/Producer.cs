8d432c782736c0a585cf395e3ae1f41880f7220d
KhomenkoMac <m.khomenko03@gmail.com>
2021-04-30 22:32:48 +0300

    add: access to data sets for abstract producer

--- Progbase3/Generator/EnityRandomProducers/Producer.cs
+++ Progbase3/Generator/EnityRandomProducers/Producer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,5 +15,61 @@ namespace Generator.EnityRandomProducers
 			_fakeDataSource = @"..\..\..\..\..\data\Generator\";
 		}
 		public abstract models.IEntity Create();
+
+		// delegates for fake-dataset interaction
+		private protected Func<string, List<string>> ReadByLines = (fullPath =>
+		{
+			List<string> lineStorage = new List<string>();
+
+			using (FileStream fs = new FileStream(fullPath, FileMode.Open, FileAccess.Read))
+			{
+				using (StreamReader sr = new StreamReader(fs))
+				{
+					string buff = String.Empty;
+					do
+					{
+						buff = sr.ReadLine();
+						if (buff == null)
+							break;
+
+						lineStorage.Add(buff);
+
+					} while (true);
+
+				}
+			}
+			return lineStorage;
+		});
+
+		private protected Func<string, List<string>> ReadBySentences = new Func<string, List<string>>(fullPath =>
+		{
+			List<string> _sentences = new List<string>();
+
+			using (FileStream fs = new FileStream(fullPath, FileMode.Open, FileAccess.Read))
+			{
+				using (StreamReader sr = new StreamReader(fs))
+				{
+					StringBuilder sb = new StringBuilder();
+
+					string buff = String.Empty;
+					do
+					{
+						buff = sr.ReadLine();
+
+						if (buff == null)
+							break;
+
+						sb.Append(buff);
+					}
+					while (true);
+
+					foreach (var sen in sb.ToString().Split('.'))
+					{
+						_sentences.Add(sen);
+					}
+				}
+			}
+			return _sentences;
+		}
 	}
 }
