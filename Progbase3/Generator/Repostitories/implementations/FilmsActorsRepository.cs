1175528d50c1ff5186fb79c01630edc5b290f77d
KhomenkoMac <m.khomenko03@gmail.com>
2021-05-01 21:08:44 +0300

    add: film-actors cross table

---
+++ Progbase3/Generator/Repostitories/implementations/FilmsActorsRepository.cs
@@ -0,0 +1,38 @@
+﻿using Generator.models;
+using Generator.Repostitories.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generator.Repostitories.implementations
+{
+	public class FilmsActorsRepository : IFilmsActorsRepository
+	{
+		public void Delete(int idEntityToDelete)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<FilmsActors> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+
+		public FilmsActors GetById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Insert(FilmsActors entityToInsert)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update(FilmsActors entityToUpdate)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Progbase3/Generator/models/FilmsActors.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generator.models
+{
+	public class FilmsActors: IEntity
+	{
+		public int Id { get; set; }
+		public int FilmId { get; set; }
+		public int ActorId { get; set; }
+	}
+}
