65c6f6b35a2fb3310053c01a539ea6643f1b6253
KhomenkoMac <m.khomenko03@gmail.com>
2021-05-07 19:51:54 +0300

    add:class for film import

---
+++ Progbase3/Generator/Import.cs
@@ -0,0 +1,27 @@
+﻿using Generator.models;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Generator
+{
+	public static class Import
+	{
+		private static XmlSerializer _XMLserializer;
+
+		static Import()
+		{
+			_XMLserializer = new XmlSerializer(typeof(IEnumerable<Film>));
+		}
+
+		public static IEnumerable<Film> ImportExportedFilms(string sourceFilmsXMLFile, string destinationDirectory)
+		{
+			IEnumerable<Film> _filmsFromFile;
+			using (FileStream fs = new FileStream(sourceFilmsXMLFile, FileMode.Open, FileAccess.Read))
+			{
+				_filmsFromFile = (IEnumerable<Film>)_XMLserializer.Deserialize(fs);
+			}
+			return _filmsFromFile;
+		}
+	}
+}
