5bbcd873e9977e8340962f4cb5063239acaa334b
KhomenkoMac <m.khomenko03@gmail.com>
2021-05-08 18:13:59 +0300

    add: map buttons,images

---
+++ Progbase3/MovieHubManagmentSystem/Views/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MovieHubManagmentSystem.Views
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		 
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			
+			try
+			{
+				DirectoryInfo di = new("../../../../../images");
+				Icon = new BitmapImage(new Uri(di.FullName + @"/icons/favicon.ico", UriKind.Absolute));
+				this.filmsImage.Source = new BitmapImage(new Uri(di.FullName + @"/pictures/filmsLogo.png", UriKind.Absolute));
+				this.actorsImage.Source = new BitmapImage(new Uri(di.FullName + @"/pictures/actorsLogo.png", UriKind.Absolute));
+				this.reviewsImage.Source = new BitmapImage(new Uri(di.FullName + @"/pictures/reviewsLogo.png", UriKind.Absolute));
+			}
+			catch (Exception err)
+			{
+				MessageBox.Show(err.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+				this.Close();
+			}
+			
+		}
+	}
+}
