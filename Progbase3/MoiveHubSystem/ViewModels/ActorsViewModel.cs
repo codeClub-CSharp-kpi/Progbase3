f450dd3111d82a7935b139216fef64e86d40be69
KhomenkoMac <m.khomenko03@gmail.com>
2021-05-23 19:52:50 +0300

    add: viewmodel for actors with list out put

--- Progbase3/MoiveHubSystem/ViewModels/ActorsViewModel.cs
+++ Progbase3/MoiveHubSystem/ViewModels/ActorsViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Generator.models;
+using Generator.Repostitories.implementations;
+using Generator.Repostitories.interfaces;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -12,13 +14,31 @@ namespace MoiveHubSystem.ViewModels
 {
 	class ActorsViewModel: INotifyPropertyChanged
 	{
+		const int AmountOfInPageElements = 5;
+		
+		private ActorRepository _repo;
+
 		public ObservableCollection<Actor> Actors { get; set; }
 
 		public ActorsViewModel()
 		{
-			
+			_repo = new ActorRepository();
+			Actors = new ObservableCollection<Actor>();
+
+			foreach (var item in GetPageForList(_currentPageCounter))
+			{
+				Actors.Add(item);
+			}
 		}
 
+		// current page counter
+		private int _currentPageCounter = 1;
+
+		// supporting private methods
+		private IEnumerable<Actor> GetPageForList(int pageNumber)
+		{
+			return _repo.GetPage(AmountOfInPageElements, AmountOfInPageElements * (pageNumber - 1));
+		}
 
 		public event PropertyChangedEventHandler PropertyChanged;
 		public void OnPropertyChanged([CallerMemberName] string prop = "")
