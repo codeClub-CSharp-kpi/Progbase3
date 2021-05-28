863bde081282c9872fc52004b415d5bb16389923
KhomenkoMac <m.khomenko03@gmail.com>
2021-05-28 13:52:01 +0300

    add: import films functions to 'Import' class

--- Progbase3/Generator/Import.cs
+++ Progbase3/Generator/Import.cs
@@ -2,19 +2,43 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Xml.Serialization;
+using System.Linq;
+using Generator.Repostitories.interfaces;
+using Generator.Repostitories.implementations;
 
 namespace Generator
 {
 	public static class Import
 	{
 		private static XmlSerializer _XMLserializer;
+		private static IFilmRepository _filmsRepo;
+		// window with imported enitites...: add 'report'list where saved only imported films
 
 		static Import()
 		{
 			_XMLserializer = new XmlSerializer(typeof(IEnumerable<Film>));
+			_filmsRepo = new FilmRepository();
 		}
 
-		public static IEnumerable<Film> ImportExportedFilms(string sourceFilmsXMLFile, string destinationDirectory)
+		public static void ImportExportedFilms(string sourceFilmsXMLFile)
+		{
+			var filmsFromXML = GetFilmsFromFile(sourceFilmsXMLFile);
+
+			foreach (var item in filmsFromXML)
+			{
+				try
+				{
+					_filmsRepo.GetById(item.Id);
+				}
+				catch (System.Exception)
+				{
+					_filmsRepo.Insert(item);
+					// can be added a dialog window with imported enitites
+				}
+			}
+		}
+
+		private static IEnumerable<Film> GetFilmsFromFile(string sourceFilmsXMLFile)
 		{
 			IEnumerable<Film> _filmsFromFile;
 			using (FileStream fs = new FileStream(sourceFilmsXMLFile, FileMode.Open, FileAccess.Read))
@@ -23,5 +47,7 @@ namespace Generator
 			}
 			return _filmsFromFile;
 		}
+
+		
 	}
 }
