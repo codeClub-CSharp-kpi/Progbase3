b6947c52fea7b31912a7106a38adb8590a461fa2
Khomenko Mac <maksim_khomenko@ukr.net>
2021-06-17 14:50:09 +0300

    refactor: sha hasher managed to hashers lib

---
+++ Progbase3/HashersLibrary/SHA256Generator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace HashersLibrary
+{
+	public static class SHA256Generator
+	{
+		public static string ProduceSHA256Hash(string sourceStr)
+		{
+			string hash = null;
+			using (SHA256 shaHash = SHA256.Create())
+			{
+				// Byte array representation of source string
+				byte[] sourceBytes = Encoding.UTF8.GetBytes(sourceStr);
+
+				// Generate hash value(Byte Array) for input data
+				byte[] hashBytes = shaHash.ComputeHash(sourceBytes);
+
+				// Convert hash byte array to string
+				hash = BitConverter.ToString(hashBytes).Replace("-", string.Empty);
+			}
+
+			return hash;
+		}
+	}
+}
